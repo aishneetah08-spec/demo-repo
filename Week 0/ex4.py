d24f72cedcdfc209a3b2a0555ed1e835cebb6cc6
Qaanitat Abubakar <aishneetah08@gmail.com>
2026-07-04 16:20:15 +0100

    added ex4

---
+++ Week 0/ex4.py
@@ -0,0 +1,9 @@
+name = "Aishat-Qaanitat"
+age = 15
+cars = 20
+isFemale = True
+
+print("My name is", name)
+print("I am", age, "years old")
+print("I have", cars, "cars")
+print("I am female:", isFemale)
